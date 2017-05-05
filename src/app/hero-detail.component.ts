143d0012db40b54d9924ceeb034e44d5d9895675
akm <akm2000@gmail.com>
2017-05-05 19:49:51 +0900

    Take the id parameter from the params Observable in the ActivatedRoute service and use the HeroService to fetch the hero with that id.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
 import { Hero } from './hero';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'hero-detail',
@@ -14,6 +19,18 @@ import { Hero } from './hero';
     </div>
   `
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+    ngOnInit(): void {
+        this.route.params
+            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+            .subscribe(hero => this.hero = hero);
+    }
+
     @Input() hero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {}
 }
