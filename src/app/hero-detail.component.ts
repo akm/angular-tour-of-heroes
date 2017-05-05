0bda332a7a3d554e5efd10c892e804c55aa6af55
akm <akm2000@gmail.com>
2017-05-05 20:36:53 +0900

    Find the way back

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -8,17 +8,8 @@ import { HeroService }         from './hero.service';
 import 'rxjs/add/operator/switchMap';
 
 @Component({
-  selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+    selector: 'hero-detail',
+    templateUrl: './hero-detail.component.html',
 })
 export class HeroDetailComponent implements OnInit {
     ngOnInit(): void {
@@ -34,4 +25,8 @@ export class HeroDetailComponent implements OnInit {
         private route: ActivatedRoute,
         private location: Location
     ) {}
+
+    goBack(): void {
+        this.location.back();
+    }
 }
