fd4149576806f7ff197d9053f26b516ee221c123
akm <akm2000@gmail.com>
2017-05-05 19:34:19 +0900

    Configure routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -23,3 +24,10 @@ import { HeroService }         from './hero.service';
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
+RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+])
