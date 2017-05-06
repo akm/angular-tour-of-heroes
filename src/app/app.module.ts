774d4cd1c73b222ed363f8bbf83a80a3edd93ea8
akm <akm2000@gmail.com>
2017-05-06 13:28:45 +0900

    Modify import order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }   from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 import { DashboardComponent }  from './dashboard.component';
-import { AppRoutingModule }    from './app-routing.module';
 
 @NgModule({
     imports:      [
